07581da936dcdac8c7b6c41dd79a790d8c179f49
Putranugroho14 <putranugroho61@gmail.com>
2024-05-21 18:39:34 +0700

    void pasien

--- asosiasi/asosiasi.cpp
+++ asosiasi/asosiasi.cpp
@@ -36,7 +36,13 @@ public:
 void pasien::tambahDokter(dokter* pDokter) {
     daftar_dokter.push_back(pDokter);
 }
-
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
 
 
 
