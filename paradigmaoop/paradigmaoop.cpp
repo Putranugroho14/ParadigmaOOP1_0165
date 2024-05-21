c7a8a5b7fd3633ad90fa89e28fff548bd2dd11cf
Putranugroho14 <putranugroho61@gmail.com>
2024-05-21 10:38:08 +0700

    membuat int main

--- paradigmaoop/paradigmaoop.cpp
+++ paradigmaoop/paradigmaoop.cpp
@@ -12,9 +12,15 @@ public:
 	Mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai) { //membuat definisi
 
 	}
+
+	~Mahasiswa() {
+		cout << "Id  = " << id << endl;
+		cout << "Nama  = " << nama << endl;
+	}
 };
 int main()
 {
-
+	Mahasiswa mhs(12, "Asroni", 90.5);
+	return 0;
 }
 
